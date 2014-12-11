f0bc9cd8915158b0e64c1b091542d9ac2200d567
Monty Lennie <montylennie@gmail.com>
2014-12-11 18:30:13 +0100

    update user traits

---
+++ db/migrate/20141211172803_add_traits_to_users.rb
@@ -0,0 +1,25 @@
+class AddTraitsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :last_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :referrer_id, :integer
+    add_index :users, :referrer_id
+    add_column :users, :referrer_paid, :boolean
+    add_column :users, :phone, :string
+    add_column :users, :birth_date, :date
+    add_column :users, :gender, :string
+    add_index :users, :gender
+    add_column :users, :wallet_id, :integer
+    add_index :users, :wallet_id
+    add_column :users, :street, :string
+    add_column :users, :district, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :country, :string
+    add_column :users, :zipcode, :string
+    add_column :users, :geocoded_address, :string
+    add_column :users, :lat, :integer
+    add_column :users, :lng, :integer
+    add_column :users, :archived, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211172003) do
+ActiveRecord::Schema.define(version: 20141211172803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -262,12 +262,33 @@ ActiveRecord::Schema.define(version: 20141211172003) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "last_name"
+    t.string   "first_name"
+    t.integer  "referrer_id"
+    t.boolean  "referrer_paid"
+    t.string   "phone"
+    t.date     "birth_date"
+    t.string   "gender"
+    t.integer  "wallet_id"
+    t.string   "street"
+    t.string   "district"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "zipcode"
+    t.string   "geocoded_address"
+    t.integer  "lat"
+    t.integer  "lng"
+    t.boolean  "archived"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["gender"], name: "index_users_on_gender", using: :btree
+  add_index "users", ["referrer_id"], name: "index_users_on_referrer_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+  add_index "users", ["wallet_id"], name: "index_users_on_wallet_id", using: :btree
 
   create_table "wallets", force: true do |t|
     t.float    "balance"

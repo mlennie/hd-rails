dd60136279f82693ab3cae7e04e6fd64e68c3f2f
Monty Lennie <montylennie@gmail.com>
2014-12-11 15:03:31 +0100

    add associations between users and roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :recoverable, :rememberable, :trackable,
          :validatable, :confirmable, :lockable
+
+  has_many :user_roles
+  has_many :roles, through: :user_roles
 end

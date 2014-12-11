9b57138000348c45953132eb37bdf1999a5fc2a6
Monty Lennie <montylennie@gmail.com>
2014-12-11 17:29:15 +0100

    add services table

---
+++ db/migrate/20141211162814_create_services.rb
@@ -0,0 +1,18 @@
+class CreateServices < ActiveRecord::Migration
+  def change
+    create_table :services do |t|
+      t.integer :availabilities
+      t.datetime :start_time
+      t.datetime :last_booking_time
+      t.integer :restaurant_id
+      t.integer :nb_10
+      t.integer :nb_15
+      t.integer :nb_20
+      t.integer :nb_25
+      t.boolean :archived
+
+      t.timestamps
+    end
+    add_index :services, :restaurant_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211162243) do
+ActiveRecord::Schema.define(version: 20141211162814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,22 @@ ActiveRecord::Schema.define(version: 20141211162243) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "services", force: true do |t|
+    t.integer  "availabilities"
+    t.datetime "start_time"
+    t.datetime "last_booking_time"
+    t.integer  "restaurant_id"
+    t.integer  "nb_10"
+    t.integer  "nb_15"
+    t.integer  "nb_20"
+    t.integer  "nb_25"
+    t.boolean  "archived"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "services", ["restaurant_id"], name: "index_services_on_restaurant_id", using: :btree
 
   create_table "user_roles", force: true do |t|
     t.integer  "user_id"

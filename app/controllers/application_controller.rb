a968782b58471ba6965cab6d1f03de6e8f649d7d
Monty Lennie <montylennie@gmail.com>
2014-12-12 15:09:01 +0100

    take active admin custom method out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def authenticate_admin_user!
-    authenticate_user!
-    unless current_user.is_superadmin?
-      flash[:alert] = "Unauthorized Access!"
-      redirect_to new_user_session_path
-    end
-  end
 end

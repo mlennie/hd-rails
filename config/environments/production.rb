4eb7794cc70b683897d153bcb973b56a80a1647e
Monty Lennie <montylennie@gmail.com>
2014-12-12 15:58:39 +0100

    change from email for devise emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
   # Do not dump schema after migrations.

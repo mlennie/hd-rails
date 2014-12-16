54215ac9bb470c84a64446bfd04b8ded6502d76d
Monty Lennie <montylennie@gmail.com>
2014-12-16 20:54:43 +0100

    update users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      head 204
+      render json: user, status: 201
     else
       render json: user.errors, status: 422
     end
@@ -12,5 +12,6 @@ end
 
 private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :first_name, :last_name)
   end

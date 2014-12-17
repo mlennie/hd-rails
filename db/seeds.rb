389976b88c5b0d2df209376ac4c2125a32c628fa
Monty Lennie <montylennie@gmail.com>
2014-12-17 19:34:53 +0100

    change admin email and password and put admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,18 @@
-
-superadmin = Role.create(
-  name: "superadmin",
-  description: "Can do everything!"
+AdminUser.create!(
+  email: 'admin@happydining.fr', 
+  password: 'happydine421', 
+  password_confirmation: 'happydine421'
 )
 
-puts 'created superadmin role'
+puts 'created admin user'
 
-admin = User.create(
+User.create(
   :email => "superman@gmail.com",
   :password => "kryptonring",
   :password_confirmation => "kryptonring",
-  :confirmed_at => Time.now, :confirmation_token => '1234567890')
-
-User.create([
-  {email: 'green@mail.com',
-   password: '12345678', password_confirmation: '12345678'},
-  {email: 'pink@mail.com',
-   password: '12345678', password_confirmation: '12345678'}
-])
+  :confirmed_at => Time.now, :confirmation_token => '1234567890'
+)
 
 puts 'created user'
-
-admin.roles << superadmin
-
-puts 'made user admin'
 
 

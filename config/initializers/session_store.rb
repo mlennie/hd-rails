a684734694e471c15bcf1e7679b37908eccd59a4
Monty Lennie <montylennie@gmail.com>
2014-12-17 18:31:03 +0100

    get active admin signup working

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-#Rails.application.config.session_store :cookie_store, key: '_hdrails_session'
-Rails.application.config.session_store :disabled
+Rails.application.config.session_store :cookie_store, key: '_hdrails_session'
+#Rails.application.config.session_store :disabled
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'sessions' }
+  devise_for :users, ActiveAdmin::Devise.config#, controllers: { sessions: 'sessions' }
   ActiveAdmin.routes(self)
   root 'admin/dashboard#index'
 

a60d1b26e8efad402d10149d5892d2b627795094
Monty Lennie <montylennie@gmail.com>
2014-12-18 09:42:38 +0100

    add index, show, edit and create fields for promotions and add archiving

--- app/admin/promotion.rb
+++ app/admin/promotion.rb
@@ -1,3 +1,59 @@
 ActiveAdmin.register Promotion do
-  permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :code, :description, :amount, :percent, :new_user_only,
+  							:expiry_date, :usage_limit, :times_used
+
+  controller do
+    def scoped_collection
+      Promotion.get_unarchived
+    end
+
+    def destroy
+      r = Promotion.find(params[:id])
+      r.archive
+      flash[:success] = "You have successfully archived this resource"
+      redirect_to admin_promotions_path
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :code
+    column :description
+    column :amount 
+    column :percent
+    column :new_user_only
+    column :expiry_date
+    column :usage_limit
+    column :times_used
+    column :created_at
+    actions
+ end
+
+  filter :name
+  filter :code
+  filter :description
+  filter :amount 
+  filter :percent
+  filter :new_user_only
+  filter :expiry_date
+  filter :usage_limit
+  filter :times_used
+  filter :created_at
+
+  form do |f|
+    f.inputs "Promotion Details" do
+      f.input :name
+	    f.input :code
+	    f.input :description
+	    f.input :amount 
+	    f.input :percent
+	    f.input :new_user_only
+	    f.input :expiry_date
+	    f.input :usage_limit
+	    f.input :times_used
+    end
+    f.actions
+ end
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,7 @@
 class Promotion < ActiveRecord::Base
 
+	include Archiving
+
   has_many :user_promotions
   has_many :users, through: :user_promotions
   has_many :reservations, through: :user_promotions

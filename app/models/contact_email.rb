f0047cb86e6ef2e574d5d6a8735c9bda37970cc4
Monty Lennie <montylennie@gmail.com>
2014-12-20 12:21:55 +0100

    add callback for contact emails to send emails

--- app/models/contact_email.rb
+++ app/models/contact_email.rb
@@ -1,3 +1,13 @@
 class ContactEmail < ActiveRecord::Base
 	include Archiving
+
+	after_save :send_received_emails
+
+	protected 
+
+		def send_received_emails
+			#send user email of acknowledgment
+			#send email to admin
+			puts sent emails!
+		end
 end

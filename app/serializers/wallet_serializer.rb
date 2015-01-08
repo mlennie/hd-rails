1eafaf3907edaa7b91036c8b9ff8f40bd4c8f23c
Monty Lennie <montylennie@gmail.com>
2015-01-08 21:32:20 +0100

    add concernable attributes for wallet serializer

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -1,3 +1,12 @@
 class WalletSerializer < ActiveModel::Serializer
   attributes :id, :balance, :concernable_type, :concernable_id
+
+  def attributes
+    data = super
+    data[:concernable] = {id: data[:concernable_id], type: data[:concernable_type]}
+    data.delete(:concernable_type)
+    data.delete(:concernable_id)
+    data
+  end
+
 end

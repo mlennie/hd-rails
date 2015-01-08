99de36b4af5d078f18ba3e24f90d38844bc7d959
Monty Lennie <montylennie@gmail.com>
2015-01-08 19:50:56 +0100

    take out concernable in wallet serializer

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -1,16 +1,8 @@
 class WalletSerializer < ActiveModel::Serializer
   attributes :id, :balance, :concernable_type, :concernable_id
 
-  belongs_to :concernable, polymorphic: true
+  belongs_to :user
 
   embed :ids, :include => true
-
-  def attributes
-    data = super
-    data[:concernable] = {id: data[:concernable_id], type: data[:concernable_type]}
-    data.delete(:concernable_type)
-    data.delete(:concernable_id)
-    data
-  end
 
 end

fb9ab80602bfd3e14fbae7fc9efb941c998b2c23
Monty Lennie <montylennie@gmail.com>
2015-01-09 14:15:12 +0100

    add address attributes to restaurant serializer

--- app/serializers/restaurants_serializer.rb
+++ app/serializers/restaurants_serializer.rb
@@ -1,3 +1,3 @@
 class RestaurantsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :img_url, :description, :zipcode
+  attributes :id, :name, :img_url, :description, :zipcode, :street, :city
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,35 +19,45 @@ Restaurant.create(
 	name: "Blue Nile",
   img_url: "http://www.toxel.com/wp-content/uploads/2009/06/restaurant08.jpg",
   description: "Best food ever!! soooo good",
-  zipcode: '75017'
+  zipcode: '75017',
+  city: 'Paris', 
+  street: '123 main street'
 )
 
 Restaurant.create(
   name: "Shang hi noon",
   img_url: "http://www.huahintoday.com/wp-content/uploads/2013/04/InAzia-Restaurant-II.jpg",
   description: "Fancy fancy fancy oh and did we say fancy",
-  zipcode: '75017'
+  zipcode: '75017',
+  city: 'Paris', 
+  street: '123 main street'
 )
 
 Restaurant.create(
   name: "Haute couture",
   img_url: "http://www.a-onehotel.com/pattaya/pattaya_images/300ppi/50MARITIME%20RESTAURANT.JPG",
   description: "The Lobster is to die for",
-  zipcode: '75017'
+  zipcode: '75017',
+  city: 'Paris', 
+  street: '123 main street'
 )
 
 Restaurant.create(
   name: "Brasserie Lip",
   img_url: "http://www.paris-bistro.com/culture/ecrivain/pics_ecrivain/prix_lipp.jpg",
   description: "Classy Brasserie sur Saint Germain",
-  zipcode: '75008'
+  zipcode: '75008',
+  city: 'Paris', 
+  street: '123 main street'
 )
 
 Restaurant.create(
   name: "Fouquet",
   img_url: "http://www.lucienbarriere.com/localized/image/photoelement/pj/500x240-ho-hbf-fouquet's-salle1_opt318838617194458100.jpg",
   description: "Sarkozy ate here. Enough said.",
-  zipcode: '75008'
+  zipcode: '75008',
+  city: 'Paris', 
+  street: '123 main street'
 )
 
 puts 'created restaurants'

12653027dc5df50e2786e66cada93c8a9f5431a5
Monty Lennie <montylennie@gmail.com>
2015-01-15 18:24:20 +0100

    change sanitize email to my email as happy dining one takes too long

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -1,7 +1,7 @@
 SanitizeEmail::Config.configure do |config|
-  config[:sanitized_to] =         'monty@happydining.com'
-  config[:sanitized_cc] =         'monty@happydining.com'
-  config[:sanitized_bcc] =        'monty@happydining.com'
+  config[:sanitized_to] = 'montylennie@gmail.com'
+  config[:sanitized_cc] = 'montylennie@gmail.com'
+  config[:sanitized_bcc] = 'montylennie@gmail.com'
   # run/call whatever logic should turn sanitize_email on and off in this Proc:
   config[:activation_proc] =      Proc.new { %w(staging).include?(Rails.env) }
   config[:use_actual_email_prepended_to_subject] = true         # or false

91327239f697ae079270109c008dd38d245d7cdb
Monty Lennie <montylennie@gmail.com>
2015-01-18 22:14:06 +0100

    change redirect email to montylennie@gmail.com

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -1,7 +1,7 @@
 SanitizeEmail::Config.configure do |config|
-  config[:sanitized_to] = 'montylennie@gmail.com'
-  config[:sanitized_cc] = 'montylennie@gmail.com'
-  config[:sanitized_bcc] = 'montylennie@gmail.com'
+  config[:sanitized_to] = 'system@happydining.fr'
+  config[:sanitized_cc] = 'system@happydining.fr'
+  config[:sanitized_bcc] = 'system@happydining.fr'
   # run/call whatever logic should turn sanitize_email on and off in this Proc:
   config[:activation_proc] =      Proc.new { %w(staging develremote).include?(Rails.env) }
   config[:use_actual_email_prepended_to_subject] = true         # or false

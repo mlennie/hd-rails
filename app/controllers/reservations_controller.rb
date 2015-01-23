ce56a09a65614c8376007edf63b9290f40152ac1
Monty Lennie <montylennie@gmail.com>
2015-01-23 22:28:12 +0100

    add reservations controller with create action

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,24 @@
+class ReservationsController < ApplicationController
+
+  def create
+    #build reservation
+    reservation = Reservation.new(reservation_params)
+
+    #save reservation
+    if reservation.save!
+      render json: reservation, status: 201
+    else
+      render json: reservation.errors, status: 422
+    end
+  end
+
+  private
+  
+    def reservation_params
+      params.require(:reservation).permit(
+        :nb_people, :time, :status, :restaurant_id, 
+        :user_id, :service_id, :discount, :user_contribution,
+        :booking_name
+      )
+    end
+end

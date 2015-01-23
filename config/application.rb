6708699a40fbe2afb9ad6103f7341abd87b779d0
Monty Lennie <montylennie@gmail.com>
2015-01-23 22:50:33 +0100

    change rails timezone to paris time

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ Bundler.require(*Rails.groups)
 
 module Hdrails
   class Application < Rails::Application
-     config.middleware.use Rack::Cors do
+    config.time_zone = 'Paris'
+    config.middleware.use Rack::Cors do
       allow do
         origins "*"
         resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]

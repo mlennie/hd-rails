613edbd66cfda4f76985e4dcd855d5fcc136103a
Monty Lennie <montylennie@gmail.com>
2015-01-27 14:50:09 +0100

    clean up service serializer to just get highest discount

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,5 +1,5 @@
 class ServiceSerializer < ActiveModel::Serializer
-  attributes :id, :availabilities, :start_time, :last_booking_time, :nb_10, :nb_15, :nb_20, :nb_25
+  attributes :id, :start_time, :last_booking_time, :discount
 
   def attributes
     data = super

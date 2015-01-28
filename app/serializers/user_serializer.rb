64220ff1e02bb8ba4cc6b598b2894dcc28788e7e
Monty Lennie <montylennie@gmail.com>
2015-01-28 16:15:14 +0100

    all user gender to be sent and updated

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :referral_code
+  attributes :id, :first_name, :last_name, :email, :referral_code, :gender
   has_one :wallet
   has_many :roles
   has_many :restaurants

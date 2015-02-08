50dd87dc22087cf0a154a4a9a643aeca6c529c51
Monty Lennie <montylennie@gmail.com>
2015-02-08 13:44:59 +0100

    add menu and menu items to admin and add creating, editing, viewing and deleting to both

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,10 @@
 class Menu < ActiveRecord::Base
+	include Archiving
+
+	belongs_to :restaurant
+	has_many :menu_items
+
+	validates_presence_of :restaurant_id, :name, :title, :kind
+
+	enum kind: [ :complete, :a_la_carte ]
 end

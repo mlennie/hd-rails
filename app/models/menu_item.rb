0103e48e08dfce065cc938031a44a78c4b73a107
Monty Lennie <montylennie@gmail.com>
2015-03-25 12:44:05 +0100

    remove price presence validation

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,7 +3,7 @@ class MenuItem < ActiveRecord::Base
 
 	belongs_to :menu
 
-	validates_presence_of :course, :name, :price, :menu_id
+	validates_presence_of :course, :name, :menu_id
 
 	enum course: [ :Hors_doeuvre, :entree, :principaux, :dessert, :boisson ]
 end

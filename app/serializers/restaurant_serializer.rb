46cd1145c22904dc2471891dc7c3b1844b677911
Monty Lennie <montylennie@gmail.com>
2015-03-25 15:57:03 +0100

    add latitude and longitude to restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,6 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :img_url, :description, :zipcode, :street, :city
+  attributes :id, :name, :img_url, :description, :zipcode, :street, :city,
+  					 :latitude, :longitude
   
   has_many :cuisines
 	has_many :services

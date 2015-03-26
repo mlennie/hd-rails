7717732f8c3dfa6cfaca3ea48b2b0a15048ff808
Monty Lennie <montylennie@gmail.com>
2015-03-26 11:43:18 +0100

    update serializers to get reservations and menus that are unarchived

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :img_url, :description, :zipcode, :street, :city
   has_many :menus
+
+  def menus
+    Menu.where(restaurant_id: self.id).get_unarchived
+  end
 end

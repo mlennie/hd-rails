2bf5753fa51bab33959bde76037b517e3f814890
Monty Lennie <montylennie@gmail.com>
2015-05-27 17:09:40 +0200

    add callbacks to create restaurant balance payment transaction

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,7 +3,10 @@ class Invoice < ActiveRecord::Base
 	include Archiving
 
   belongs_to :restaurant
-  belongs_to :invoice_transaction, class_name: :transaction
+  has_many :transactions, as: :itemable
+
+  before_update :check_if_unpaying
+  after_update :add_transaction_if_paid
 
   def self.make_new invoice_data
   	invoice = Invoice.new({
@@ -44,4 +47,31 @@ class Invoice < ActiveRecord::Base
   		return false
   	end
   end
+
+  protected
+
+    #throw error if trying to change to unpaid, since code needs to be 
+    #added to the add_transaction_if_paid method below to archive last transacton
+    #and reset balance when removing paid status
+    def check_if_unpaying
+      return false if self.paid_changed? && self.paid == false
+    end
+
+    #callback after updating. If invoice has been set to paid, create transaction
+    def add_transaction_if_paid
+      #don't make a transaction if there is already a transaction
+      unless self.transactions.get_unarchived.any?
+        if self.paid?
+          transaction_params = {}
+          transaction_params[:restaurant] = Restaurant.find(self.restaurant_id)
+          transaction_params[:invoice] = self
+          transaction_params[:positive] = true
+          transaction_params[:amount] = self.final_balance.abs
+          Transaction.create_balance_payment_transaction(transaction_params)
+        else #archive last transaction and reset balance since invoice is no
+            #longer paid (paid status has been removed)
+            #TO DO 
+        end
+      end
+    end
 end
